ddf195b1912bfbd386bd9ed400d007433da77942
1002020221 <1002020221@unajma.edu.pe>
2025-06-25 12:16:40 -0500

    Error de inicio de angular 2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,71 +1,24 @@
 // src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 
-import { routes } from './app.routes'; // Importa tus rutas definidas
-import { provideAuth } from '@angular/fire/auth';
-import { getAuth } from 'firebase/auth';
+import { routes } from './app.routes';
 import { environment } from '../enviroments/environment';
 
 export const appConfig: ApplicationConfig = {
-providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+  providers: [
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
-    // Configuración de Firebase
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
     provideStorage(() => getStorage()),
   ],
 };
-
-function provideZoneChangeDetection(arg0: { eventCoalescing: boolean; }): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
-   throw new Error('Function not implemented.');
-}
-
-
-function provideClientHydration(arg0: any): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
-   throw new Error('Function not implemented.');
-}
-
-
-function withEventReplay(): any {
-   throw new Error('Function not implemented.');
-}
-
-
-function provideHttpClient(): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
-   throw new Error('Function not implemented.');
-}
-
-
-function provideFirebaseApp(arg0: () => any): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
-   throw new Error('Function not implemented.');
-}
-
-
-function initializeApp(firebaseConfig: any) {
-   throw new Error('Function not implemented.');
-}
-
-
-function provideFirestore(arg0: () => any): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
-   throw new Error('Function not implemented.');
-}
-
-
-function getFirestore() {
-   throw new Error('Function not implemented.');
-}
-
-
-function provideStorage(arg0: () => any): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
-   throw new Error('Function not implemented.');
-}
-
-
-function getStorage() {
-   throw new Error('Function not implemented.');
-}
