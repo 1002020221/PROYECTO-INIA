49ec8cdc6efa6dc92824ab067559b65164584ccf
1002020221 <1002020221@unajma.edu.pe>
2025-06-25 21:10:07 -0500

    nueva funcion para autenticar en login.service.ts

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -8,6 +8,10 @@ export class LoginService {
 
   constructor(private auth:Auth) { }
   login(email: string, password: string): Promise<UserCredential> {
-    return signInWithEmailAndPassword(this.auth, email, password);
+    return signInWithPopud(this.auth, email, password);
   }
 }
+function signInWithPopud(auth: Auth, email: string, password: string): Promise<UserCredential> {
+  throw new Error('Function not implemented.');
+}
+
